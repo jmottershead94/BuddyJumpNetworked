8b11e498b69b5d740bdb6a6bed6521c7bcf85f0f
jmottershead94 <jasonmottershead@hotmail.co.uk>
2016-02-20 16:01:35 +0000

    Made the standard message a variable.

--- app/src/main/java/com/example/app/jason/ragerelease/app/Framework/AndroidOSHandlers/SMSHandler.java
+++ app/src/main/java/com/example/app/jason/ragerelease/app/Framework/AndroidOSHandlers/SMSHandler.java
@@ -5,7 +5,6 @@ package com.example.app.jason.ragerelease.app.Framework.AndroidOSHandlers;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.provider.MediaStore;
 
 /**
  * Created by Jason Mottershead on 20/02/2016.
@@ -15,13 +14,18 @@ import android.provider.MediaStore;
 // This class will make use of the Android's own sms application.
 public class SMSHandler extends Activity
 {
+    // Attributes.
+    // Private.
+    private final String standardMessage = "I have played Buddy Jump, you know, that really cool and awesome game in development by 1300455. Yeah...";
+
+    // Methods.
     public SMSHandler(final Activity gameOverScreen)
     {
         // An intent that takes us to an sms activity.
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:"));
 
         // The default message to the SMS application.
-        intent.putExtra("sms_body", "I have played Buddy Jump, you know, that really cool and awesome game in development by 1300455. Yeah...");
+        intent.putExtra("sms_body", standardMessage);
 
         // This should go to the sms app.
         gameOverScreen.startActivity(intent);

a8d97c66ca37e96145dbca59609c1216509eedfe
jmottershead94 <jasonmottershead@hotmail.co.uk>
2016-04-20 00:40:43 +0100

    Added in a static method to create a message box and static variables to track user response with the message box.

--- app/src/main/java/com/example/app/jason/ragerelease/app/Framework/Debug/DebugInformation.java
+++ app/src/main/java/com/example/app/jason/ragerelease/app/Framework/Debug/DebugInformation.java
@@ -3,6 +3,9 @@ package com.example.app.jason.ragerelease.app.Framework.Debug;
 
 // All of the extra includes here.
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.widget.Toast;
 
 /**
@@ -12,6 +15,12 @@ import android.widget.Toast;
 // This class will contain methods which will allow debug information to be easily printed.
 public class DebugInformation
 {
+    // Attributes.
+    public static final int NO_MESSAGE_BOX = 0;         // This is the result number used to show that there is no message box currently.
+    public static final int ACCEPTED_MESSAGE = 1;       // This will tell us if the user has pressed the positive option from the message box.
+    public static final int DECLINED_MESSAGE = 2;       // This will tell us if the user has pressed the negative option from the message box.
+    public static int messageReply = NO_MESSAGE_BOX;    // Initialising the current value of the user reply to no message box.
+
     // Methods.
     //////////////////////////////////////////////////
     //          Display Short Toast Message         //
@@ -31,5 +40,80 @@ public class DebugInformation
     public static void displayLongToastMessage(final Activity currentActivity, final String toastMessage)
     {
         Toast.makeText(currentActivity.getApplicationContext(), toastMessage, Toast.LENGTH_LONG).show();
+    }
+
+    //////////////////////////////////////////////////
+    //              Display Message Box             //
+    //==============================================//
+    // This will display a message box to the user  //
+    // asking them a question.                      //
+    // We can also track the response of the reply  //
+    // using the messageReply attribute.            //
+    //////////////////////////////////////////////////
+    public static void displayMessageBox(final Context context, final String title, final String message, final String positiveOption, final String negativeOption)
+    {
+        // Create the alert box.
+        AlertDialog.Builder alertBox = new AlertDialog.Builder(context);
+
+        // Setting the title of the message box.
+        alertBox.setTitle(title);
+
+        // Setting the message for the message box.
+        alertBox.setMessage(message);
+
+        // Setting whether or not we can cancel out from this message box or not.
+        alertBox.setCancelable(false);
+
+        // This will set the position option for the message box.
+        alertBox.setPositiveButton(positiveOption, new DialogInterface.OnClickListener()
+        {
+            //////////////////////////////////////////////////
+            //                  On Click                    //
+            //==============================================//
+            // This will define what happens when we click  //
+            // on this option.                              //
+            // Here we set the value of the message reply   //
+            // to the same value as an accepted message to  //
+            // track user response.                         //
+            //////////////////////////////////////////////////
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                // We have messageReply the message box.
+                messageReply = ACCEPTED_MESSAGE;
+
+                // Remove the message box from the UI.
+                dialogInterface.cancel();
+            }
+        });
+
+        // This will set the negative option for the message box.
+        alertBox.setNegativeButton(negativeOption, new DialogInterface.OnClickListener()
+        {
+            //////////////////////////////////////////////////
+            //                  On Click                    //
+            //==============================================//
+            // This will define what happens when we click  //
+            // on this option.                              //
+            // Here we set the value of the message reply   //
+            // to the same value as an declined message to  //
+            // track user response.                         //
+            //////////////////////////////////////////////////
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                // We have declined the message box.
+                messageReply = DECLINED_MESSAGE;
+
+                // Remove the message box from the UI.
+                dialogInterface.cancel();
+            }
+        });
+
+        // Create this message box.
+        AlertDialog messageBox = alertBox.create();
+
+        // Display the message box on to the screen.
+        messageBox.show();
     }
 }

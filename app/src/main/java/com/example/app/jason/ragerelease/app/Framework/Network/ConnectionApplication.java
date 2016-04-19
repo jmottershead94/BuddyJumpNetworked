adc5959ac22df7b3a0bb04b436054ad11a602f59
jmottershead94 <jasonmottershead@hotmail.co.uk>
2016-04-19 13:07:17 +0100

    Connection application fully commented.

--- app/src/main/java/com/example/app/jason/ragerelease/app/Framework/Network/ConnectionApplication.java
+++ app/src/main/java/com/example/app/jason/ragerelease/app/Framework/Network/ConnectionApplication.java
@@ -1,5 +1,7 @@
+// The package location for this class.
 package com.example.app.jason.ragerelease.app.Framework.Network;
 
+// All of the extra includes here.
 import android.app.Application;
 import android.net.wifi.WifiManager;
 import android.net.wifi.p2p.WifiP2pManager;
@@ -12,49 +14,36 @@ import com.example.app.jason.ragerelease.app.Framework.Player;
  */
 
 // Connection application IS AN application, therefore extends from it.
-// This class should hopefully allow us to keep/maintain a WiFi connection between
-// different activities.
+// This class should allow us to keep/maintain a WiFi connection between different activities.
 public class ConnectionApplication extends Application
 {
     // Attributes.
-    private static ConnectionApplication connectionApplication = null;
-    private ConnectionManagement connectionManagement = null;
+    // Private.
+    private ConnectionManagement connectionManagement = null;           // This will manage our connections and should keep values between activities.
 
     // Methods.
-    @Override
-    public void onCreate()
-    {
-        super.onCreate();
-        connectionApplication = this;
-    }
-
-    // This will return.
-    public static ConnectionApplication getConnectionApplication() { return connectionApplication; }
-
     // Setters.
-    public void setConnectionManagement(WifiP2pManager p2pManager, WifiManager manager, Channel channel, NetworkActivity activity)
-    {
-        connectionManagement = new ConnectionManagement(p2pManager, manager, channel, activity);
-    }
+    // This will set up our connection manager.
+    public void setConnectionManagement(WifiP2pManager p2pManager, WifiManager manager, Channel channel, NetworkActivity activity) { connectionManagement = new ConnectionManagement(p2pManager, manager, channel, activity); }
+
+    // This will set up our server player instance.
+    // Which will be used in order to track player tapping.
+    public void setServerPlayer(Player gamePlayer)                                                                                  { connectionManagement.getWifiHandler().getWifiP2PBroadcastReceiver().getServerTask().setPlayer(gamePlayer); }
+
+    // This will set up our client player instance.
+    // Which will be used in order to track the player tapping.
+    public void setClientPlayer(Player gamePlayer)                                                                                  { connectionManagement.getWifiHandler().getWifiP2PBroadcastReceiver().getClientTask().setPlayer(gamePlayer); }
 
     // Getters.
-    // This will return our current instance of connection management.
-    public ConnectionManagement getConnectionManagement() { return connectionManagement; }
+    // This will return our connection management instance.
+    public ConnectionManagement getConnectionManagement()   { return connectionManagement; }
 
     // This will return our server image index.
-    public int getServerPeerIndexImage() { return connectionManagement.getWifiHandler().getWifiP2PBroadcastReceiver().getServerTask().getPeerImageIndexInt(); }
+    public int getServerPeerIndexImage()                    { return connectionManagement.getWifiHandler().getWifiP2PBroadcastReceiver().getServerTask().getPeerImageIndexInt(); }
 
     // This will return our client image index.
-    public int getClientPeerIndexImage() { return connectionManagement.getWifiHandler().getWifiP2PBroadcastReceiver().getClientTask().getPeerImageIndexInt(); }
+    public int getClientPeerIndexImage()                    { return connectionManagement.getWifiHandler().getWifiP2PBroadcastReceiver().getClientTask().getPeerImageIndexInt(); }
 
-    public void setServerPlayer(Player gamePlayer)
-    {
-        connectionManagement.getWifiHandler().getWifiP2PBroadcastReceiver().getServerTask().setPlayer(gamePlayer);
-    }
 
-    public void setClientPlayer(Player gamePlayer)
-    {
-        connectionManagement.getWifiHandler().getWifiP2PBroadcastReceiver().getClientTask().setPlayer(gamePlayer);
-    }
 
 }
